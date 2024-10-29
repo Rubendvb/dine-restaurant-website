373fdb7cb365bfcc16705e014de8e1a5350a77d6
Rubendvb <rubenvasquez556@gmail.com>
2024-10-29 00:10:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Booking } from './pages/Booking'
+import { Home } from './pages/Home'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Booking />
+    </>
+  )
+}
+
+export default App
