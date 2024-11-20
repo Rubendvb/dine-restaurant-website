4de6f487aa1784af863c72f00bdbdadc0faf2592
Rubendvb <rubenvasquez556@gmail.com>
2024-11-20 20:45:56 -0300

    familyExperience desktop

--- src/components/FamilyExperience/index.tsx
+++ src/components/FamilyExperience/index.tsx
@@ -21,15 +21,16 @@ export function FamilyExperience() {
           />
         </picture>
 
-        <img className="family-experience-divide" src={Divide} alt="" />
-
-        <h2 className="family-experience-title">
-          Enjoyable place for all the family
-        </h2>
-        <p className="family-experience-text">
-          Our relaxed surroundings make dining with us a great experience for
-          everyone. We can even arrange a tour of the farm before your meal.
-        </p>
+        <div className="family-experience-content">
+          <img className="family-experience-divide" src={Divide} alt="" />
+          <h2 className="family-experience-title">
+            Enjoyable place for all the family
+          </h2>
+          <p className="family-experience-text">
+            Our relaxed surroundings make dining with us a great experience for
+            everyone. We can even arrange a tour of the farm before your meal.
+          </p>
+        </div>
       </article>
 
       <article className="family-experience-place">
@@ -48,16 +49,17 @@ export function FamilyExperience() {
           />
         </picture>
 
-        <img className="family-experience-divide" src={Divide} alt="" />
-
-        <h2 className="family-experience-title">
-          The most locally sourced food
-        </h2>
-        <p className="family-experience-text">
-          All our ingredients come directly from our farm or local fishery. So
-          you can be sure that you’re eating the freshest, most sustainable
-          food.
-        </p>
+        <div className="family-experience-content content-two">
+          <img className="family-experience-divide" src={Divide} alt="" />
+          <h2 className="family-experience-title">
+            The most locally sourced food
+          </h2>
+          <p className="family-experience-text">
+            All our ingredients come directly from our farm or local fishery. So
+            you can be sure that you’re eating the freshest, most sustainable
+            food.
+          </p>
+        </div>
       </article>
     </section>
   )
