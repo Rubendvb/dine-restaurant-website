e849e5cde70c30a6692f668086cb30aaa521d1e2
Rubendvb <rubenvasquez556@gmail.com>
2024-11-26 10:24:45 -0300

    style desktop

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -7,43 +7,60 @@ import './Events.scss'
 
 export function Events() {
   const [activeIndex, setActiveIndex] = useState(0)
+  const [imageDesktop, setImageDesktop] = useState(
+    '/images/homepage/family-gathering-desktop@2x.jpg'
+  )
+  const [imageTablet, setImageTablet] = useState(
+    '/images/homepage/family-gathering-tablet@2x.jpg'
+  )
+  const [imageMobile, setImageMobile] = useState(
+    '/images/homepage/family-gathering-mobile@2x.jpg'
+  )
 
   const items = [
     {
       title: 'Family Gathering',
       description:
         'We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.',
+      imageDesktop: '/images/homepage/family-gathering-desktop@2x.jpg',
+      imageTablet: '/images/homepage/family-gathering-tablet@2x.jpg',
+      imageMobile: '/images/homepage/family-gathering-mobile@2x.jpg',
     },
     {
       title: 'Special Events',
       description:
         'Whether it’s a romantic dinner or special date you’re celebrating with others, we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.',
+      imageDesktop: '/images/homepage/special-events-desktop@2x.jpg',
+      imageTablet: '/images/homepage/special-events-tablet@2x.jpg',
+      imageMobile: '/images/homepage/special-events-mobile@2x.jpg',
     },
     {
       title: 'Social Events',
       description:
         'Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.',
+      imageDesktop: '/images/homepage/social-events-desktop@2x.jpg',
+      imageTablet: '/images/homepage/social-events-tablet@2x.jpg',
+      imageMobile: '/images/homepage/social-events-mobile@2x.jpg',
     },
   ]
 
   function handleList(index: number) {
     setActiveIndex(index)
+
+    setImageDesktop(items[index].imageDesktop)
+    setImageTablet(items[index].imageTablet)
+    setImageMobile(items[index].imageMobile)
   }
+
   return (
     <section className="events">
       <div>
         <picture>
-          <source
-            media="(min-width: 1024px)"
-            srcSet="/images/homepage/family-gathering-desktop@2x.jpg"
-          />
-          <source
-            media="(min-width: 768px)"
-            srcSet="/images/homepage/family-gathering-tablet@2x.jpg"
-          />
+          <source media="(min-width: 1024px)" srcSet={imageDesktop} />
+          <source media="(min-width: 768px)" srcSet={imageTablet} />
           <img
             className="events-image"
-            src="/images/homepage/family-gathering-mobile@2x.jpg"
+            src={imageMobile}
             alt="Family gathering"
           />
         </picture>
