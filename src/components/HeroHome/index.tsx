52b5ce3c1ad0640680557fcae967640ff165eec9
Rubendvb <rubenvasquez556@gmail.com>
2024-11-27 01:39:29 -0300

    fix header mobile

--- src/components/HeroHome/index.tsx
+++ src/components/HeroHome/index.tsx
@@ -6,27 +6,28 @@ import './HeroHome.scss'
 export function HeroHome() {
   return (
     <header className="hero">
-      <picture className="hero__img">
+      <picture className="hero__picture">
         <source
           media="(min-width: 1024px)"
           srcSet="/images/homepage/hero-bg-desktop@2x.jpg"
         />
         <source
           media="(min-width: 768px)"
-          srcSet="/images/homepage/hero-bg-tablet@2x.jpg"
+          srcSet="/images/homepage/hero-bg-tablet.jpg"
         />
-        <img
-          src="/images/homepage/hero-bg-mobile@2x.jpg"
-          alt="hero-bg-mobile"
-        />
+        <img src="/images/homepage/hero-bg-mobile.jpg" alt="hero-bg-mobile" />
       </picture>
 
       <div className="hero__content">
-        <Logo />
+        <div className="hero__content__logo">
+          <Logo />
+        </div>
 
-        <h1 className="hero__content__h1">Exquisite dining since 1989</h1>
+        <h1 className="hero__content__title">
+          Exquisite dining <br /> since 1989
+        </h1>
 
-        <p className="hero__content__p">
+        <p className="hero__content__description">
           Experience our seasonal menu in beautiful country surroundings. Eat
           the freshest produce from the comfort of our farmhouse.
         </p>
